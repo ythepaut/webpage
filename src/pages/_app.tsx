854290ee77eea5b99260d188cb84650193b371e2
Yohann THEPAUT <contact@ythepaut.com>
2021-07-30 20:12:21 +0200

    Fixed og image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import {AppProps} from "next/app";
 import Layout from "../components/layout/Layout";
 import Head from "next/head";
 import "../styles/tailwind.css";
-import banner from "../assets/img/banner.png";
 
 function App({Component, pageProps}: AppProps): JSX.Element {
     return (
@@ -16,12 +15,12 @@ function App({Component, pageProps}: AppProps): JSX.Element {
                 <meta property="og:url" content="https://www.ythepaut.com/" />
                 <meta property="og:title" content="Yohann THEPAUT" />
                 <meta property="og:description" content="Yohann THEPAUT - Page web personnelle" />
-                <meta property="og:image" content={banner} />
+                <meta property="og:image" content="/banner.png" />
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://www.ythepaut.com/" />
                 <meta property="twitter:title" content="Yohann THEPAUT" />
                 <meta property="twitter:description" content="Yohann THEPAUT - Page web personnelle" />
-                <meta property="twitter:image" content={banner} />
+                <meta property="twitter:image" content="/banner.png" />
             </Head>
             <Layout>
                 <Component {...pageProps} />
