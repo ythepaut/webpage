c75830b6e33956903f0d1ef774b26a50c36a10b5
Yohann THEPAUT <contact@ythepaut.com>
2021-09-06 15:32:52 +0100

    Fixed getStatic props in /components instead of /pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import Hero from "../sections/Hero";
 import Contact from "../sections/Contact";
 import Projects from "../sections/Projects";
+import {GetStaticProps, InferGetStaticPropsType} from "next";
 
-function HomePage(): JSX.Element {
+function HomePage({projects}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
     return (
         <div className="bg-gray-50">
             <Hero />
-            <Projects />
+            <Projects projects={projects}/>
             <Contact />
         </div>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const res = await fetch("https://gh-pinned-repos-5l2i19um3.vercel.app/?username=ythepaut");
+    const projects = await res.json();
+    return !projects ? {notFound: true} : {
+        props: {projects: projects},
+        revalidate: 1
+    };
 }
 
 export default HomePage;
