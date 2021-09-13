f89ecbe20f0ecc9bb1db7c2e5bdbd0ed085e2d11
Yohann THEPAUT <contact@ythepaut.com>
2021-09-13 14:04:01 +0100

    Added animations on scroll

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,6 +1,7 @@
 import React, {useRef, useState} from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import {useTranslations} from "use-intl";
+import Animate from "../components/Animate";
 
 function Contact({email}: {email: string}): JSX.Element {
 
@@ -19,30 +20,39 @@ function Contact({email}: {email: string}): JSX.Element {
 
     return (
         <section id="contact" className="bg-gray-200 p-4 py-10 lg:p-20 text-center">
-            <h1 className="sm:text-4xl text-2xl font-medium title-font text-gray-900 pb-6">{t("title")}</h1>
+
+            <Animate>
+                <h1 className="sm:text-4xl text-2xl font-medium title-font text-gray-900 pb-6">{t("title")}</h1>
+            </Animate>
+
             {showEmail ?
-                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                    {t.rich("info_email", {
-                        email: email,
-                        link: (children => <a className="text-blue-500 transition hover:text-blue-700" target="_blank" href={"mailto:" + children}>{children}</a>)
-                    })}
-                </p>
+                <Animate>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        {t.rich("info_email", {
+                            email: email,
+                            link: (children => <a className="text-blue-500 transition hover:text-blue-700" target="_blank" href={"mailto:" + children}>{children}</a>)
+                        })}
+                    </p>
+                </Animate>
                 :
                 <></>
             }
-            <div className={showEmail ? "invisible h-0" : "grid"}>
-                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                    {t("captcha")}
-                </p>
-                <div className="flex my-6 justify-center">
-                    <div className="w-24 h-1 rounded-full bg-gray-300 inline-flex"/>
-                </div>
-                <div className="mx-auto p-4">
-                    <div className="w-min">
-                        <ReCAPTCHA sitekey={SITE_KEY} onChange={handleCaptchaResolve} ref={recaptchaRef} />
+
+            <Animate>
+                <div className={showEmail ? "invisible h-0" : "grid"}>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        {t("captcha")}
+                    </p>
+                    <div className="flex my-6 justify-center">
+                        <div className="w-24 h-1 rounded-full bg-gray-300 inline-flex"/>
+                    </div>
+                    <div className="mx-auto p-4">
+                        <div className="w-min">
+                            <ReCAPTCHA sitekey={SITE_KEY} onChange={handleCaptchaResolve} ref={recaptchaRef} />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Animate>
         </section>
     );
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectCard from "../components/ProjectCard";
 import {useTranslations} from "use-intl";
+import Animate from "../components/Animate";
 
 function Projects(projects: any) {
 
@@ -8,29 +9,35 @@ function Projects(projects: any) {
 
     return (
         <section id="projects" className="bg-white py-10 lg:p-20">
-            <h1 className="sm:text-4xl text-2xl font-medium title-font text-gray-900 pb-6 text-center">
-                {t("title")}
-            </h1>
-            <h2 className="leading-relaxed xl:w-2/4 lg:w-3/4 mx-10 lg:mx-auto text-gray-800 text-center">
-                {t.rich("description", {
-                    br: () => <br />
-                })}
-            </h2>
+            <Animate>
+                <>
+                    <h1 className="sm:text-4xl text-2xl font-medium title-font text-gray-900 pb-6 text-center">
+                        {t("title")}
+                    </h1>
+                    <h2 className="leading-relaxed xl:w-2/4 lg:w-3/4 mx-10 lg:mx-auto text-gray-800 text-center">
+                        {t.rich("description", {
+                            br: () => <br />
+                        })}
+                    </h2>
+                </>
+            </Animate>
             <div className="flex my-6 justify-center">
                 <div className="w-24 h-1 rounded-full bg-gray-300 inline-flex"/>
             </div>
             <div className="container px-5 mx-auto">
-                <div className="flex flex-wrap">
-                    {projects.projects.map((project: any) => (
-                        <ProjectCard
-                            key={project.repo}
-                            title={project.repo}
-                            language={project.language}
-                            description={project.description}
-                            repoUrl={project.link}
-                        />
-                    ))}
-                </div>
+                <Animate>
+                    <div className="flex flex-wrap">
+                        {projects.projects.map((project: any) => (
+                            <ProjectCard
+                                key={project.repo}
+                                title={project.repo}
+                                language={project.language}
+                                description={project.description}
+                                repoUrl={project.link}
+                            />
+                        ))}
+                    </div>
+                </Animate>
             </div>
         </section>
     );
