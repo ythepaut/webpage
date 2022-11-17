8139b1338a898f9dc6c9ccee816bb6d70e3a082c
Yohann THEPAUT <contact@ythepaut.com>
2022-11-17 18:27:22 +0100

    test: fix graphql request spy implementation

--- test/pages/api/projects.spec.ts
+++ test/pages/api/projects.spec.ts
@@ -5,7 +5,14 @@ import {GraphQLClient} from "graphql-request";
 describe("Project list", () => {
     it("should fetch pinned projects from GitHub profile", async () => {
 
-        const requestSpy = jest.spyOn(GraphQLClient.prototype, "request");
+        const requestSpy = jest.spyOn(GraphQLClient.prototype, "request")
+            .mockReturnValue(Promise.resolve({
+                user: {
+                    pinnedItems: {
+                        nodes: []
+                    }
+                }
+            }));
 
         const { req, res } = createMocks({method: "GET"});
         await handler(req, res);
