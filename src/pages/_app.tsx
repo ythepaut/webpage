d9c700c2135ea3dd3dcaecc153ed22efc9888159
Yohann THEPAUT <contact@ythepaut.com>
2022-11-19 13:12:12 +0100

    feat: add name config. to meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,25 +4,29 @@ import Head from "next/head";
 import {NextIntlProvider} from "next-intl";
 import Layout from "../components/layout/Layout";
 import {AnimatePresence} from "framer-motion";
+import getConfig from "next/config";
 
 export default function App({Component, pageProps, router}: AppProps) {
+
+    const {publicRuntimeConfig} = getConfig();
+
     return (
         <>
             <Head>
-                <title>Yohann THEPAUT</title>
+                <title>{publicRuntimeConfig.name}</title>
 
-                <meta name="subject" content="Yohann THEPAUT"/>
+                <meta name="subject" content={publicRuntimeConfig.name}/>
                 <meta name="description" content="Page web personnelle"/>
                 <meta name="author" content="Yohann THEPAUT (ythepaut)"/>
                 <meta name="keywords" content="yohann thepaut, ythepaut, yohann, thepaut, page web, site web"/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:url" content="https://www.ythepaut.com/"/>
-                <meta property="og:title" content="Yohann THEPAUT"/>
+                <meta property="og:title" content={publicRuntimeConfig.name}/>
                 <meta property="og:description" content="Page web personnelle"/>
                 <meta property="og:image" content="/banner.png"/>
                 <meta property="twitter:card" content="summary_large_image"/>
                 <meta property="twitter:url" content="https://www.ythepaut.com/"/>
-                <meta property="twitter:title" content="Yohann THEPAUT"/>
+                <meta property="twitter:title" content={publicRuntimeConfig.name}/>
                 <meta property="twitter:description" content="Page web personnelle"/>
                 <meta property="twitter:image" content="/banner.png"/>
             </Head>
