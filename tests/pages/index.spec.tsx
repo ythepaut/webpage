fcdc16ed473178c50d01367e8c757248739ec51d
Yohann THEPAUT <contact@ythepaut.com>
2022-12-07 18:25:09 +0100

    test(index): wip add test index

--- tests/pages/index.spec.tsx
+++ tests/pages/index.spec.tsx
@@ -1,7 +1,34 @@
+import HomePage from "../../src/pages/index";
 import {getStaticProps} from "../../src/pages";
 import {expect} from "@jest/globals";
+import {render, screen} from "@testing-library/react";
+import {NextIntlProvider} from "next-intl";
+import "../mocks/router.mock";
+import "../mocks/intersection-observer.mock";
 
 describe("Home Page", () => {
+    it("should render", () => {
+        jest.mock("../../src/components/sections/Hero", () => ({
+            Hero: () => <>HERO</>
+        }));
+        jest.mock("../../src/components/sections/Projects", () => ({
+            Projects: () => <>PROJECTS</>
+        }));
+        jest.mock("../../src/components/sections/Contact", () => ({
+            Contact: () => <>CONTACT</>
+        }));
+
+        render(
+            <NextIntlProvider messages={{}} locale={"fr"}>
+                <HomePage />
+            </NextIntlProvider>
+        );
+
+        expect(screen.getByText("HERO")).toBeInTheDocument();
+        expect(screen.getByText("PROJECTS")).toBeInTheDocument();
+        expect(screen.getByText("CONTACT")).toBeInTheDocument();
+    });
+
     it("should fetch i18n messages", async () => {
         const response = await getStaticProps({locale: "en"}) as any;
         expect(response.props.messages).toBeTruthy();
